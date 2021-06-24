1130b98c48fccb820fabbdb12801f065970c7adc
Ken McCrory <biggmakk@outlook.com>
2021-06-24 15:47:30 -0500

    Initial Commit

---
+++ start/startApp.swift
@@ -0,0 +1,17 @@
+//
+//  startApp.swift
+//  start
+//
+//  Created by VESTA on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct startApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
